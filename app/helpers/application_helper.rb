ceef19a2a780716282d9ceb17464c58f78cd2865
DevNeoLee <devneolee@gmail.com>
2020-10-15 19:53:16 -0700

    add gravatar toastr

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+    def avatar_url(user)
+        gravatar_id = Digest::MD5::hexdigest("varavaravarvarm@gmail.com").downcase
+        "http://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
+    end
+
+    def toastr_flash
+    flash.each_with_object([]) do |(type, message), flash_messages|
+        type = 'success' if type == 'notice'
+        type = 'error' if type == 'alert'
+        text = "<script>toastr.#{type}('#{message}', '', { closeButton: true, progressBar: true })</script>"
+        flash_messages << text.html_safe if message
+    end.join("\n").html_safe
+end
 end
