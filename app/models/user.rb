90248149295204ea5af3de60a7f683a324e24437
DevNeoLee <devneolee@gmail.com>
2020-10-18 07:40:28 -0700

    add default avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { maximum: 35}
+
+  has_one_attached :avatar # 마치 유저의 속한 하나의 파일 처럼 작동
+
+  def default_avatar
+    if avatar.attached?
+      avatar
+    else 
+      'default_avatar.jpg'
+    end
+  end
+
 end
