dd1fed0c90e70f0e4c2576bccd34547cf835e8ee
DevNeoLee <devneolee@gmail.com>
2020-10-18 15:59:01 -0700

    edit registrations without password devise function

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users, path_names: { sign_in: "login", sign_out: "logout", sign_up: "signup"},
-                     controllers: { omniauth_callbacks: "omniauth_callbacks"}
+                     controllers: { omniauth_callbacks: "omniauth_callbacks",
+                                    registrations: 'registrations'
+                                  }
   
   root 'pages#home'
   
