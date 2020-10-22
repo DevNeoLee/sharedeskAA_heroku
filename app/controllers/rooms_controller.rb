c104ac800797dfbf18b772346e5656c5efb1d2f4
DevNeoLee <devneolee@gmail.com>
2020-10-22 13:49:53 -0700

    add room views/edit, new, show, images

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,7 @@
 class RoomsController < ApplicationController
   before_action :set_room, only: [:show, :edit, :update]
   before_action :authenticate_user!, except: [:show]
+  
   def index
     @rooms = current_user.rooms
   end
@@ -17,14 +18,16 @@ class RoomsController < ApplicationController
     @room = current_user.rooms.build(room_params)
     
     if @room.save 
-      redirect_to @room, notice: "Saved..."
+      redirect_to edit_room_path(@room) , notice: "Saved..."
     else
-      render :new 
+      render new_room_path , notice: "hmmmmmm...well well"
     end
   end
 
   def edit
- 
+    unless current_user.id == @room.user.id
+      redirect_to root_path, notice: "You don't have permission."
+    end
   end
 
   def update
@@ -41,6 +44,6 @@ class RoomsController < ApplicationController
     end
 
     def room_params 
-      params.require(:room).permit(:home_type, :room_type, :accomodate, :bed_room, :bath_room, :listing_name, :summary, :address, :is_wifi, :is_kitchen, :is_air, :is_heating, :is_cable, :price, :active)
+      params.require(:room).permit(:home_type, :room_type, :accomodate, :bed_room, :bath_room, :listing_name, :summary, :address, :is_wifi, :is_kitchen, :is_air, :is_heating, :is_cable, :price, :active, images: [])
     end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 import "bootstrap";
-
+import '@fortawesome/fontawesome-svg-core';
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -16,5 +16,7 @@ import "bootstrap";
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 global.toastr = require("toastr")
+FontAwesome.config.mutateApproach = 'sync'
 
 
+import { config, library, dom } from '@fortawesome/fontawesome-svg-core'
