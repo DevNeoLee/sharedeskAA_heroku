bbadb76d4eb0a4bd54d56e9a2b8be35a4cbc8d3e
DevNeoLee <devneolee@gmail.com>
2020-10-24 20:46:41 -0700

    add ajax call on datepicker reservation form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery')
 import "bootstrap";
+require("jquery-ui")
 // import '@fortawesome/fontawesome-svg-core';
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -31,3 +33,6 @@ window.dispatchMapsEvent = function (...args) {
   event.args = args
   window.dispatchEvent(event)
 }
+
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
