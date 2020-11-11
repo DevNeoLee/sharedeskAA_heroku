e6ed6f65a50156d6f1cdfb1d0a7d3191f91a021b
DevNeoLee <devneolee@gmail.com>
2020-11-10 20:17:46 -0800

    update CSS styles images bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
-    def avatar_url(user)
-        gravatar_id = Digest::MD5::hexdigest("varavaravarvarm@gmail.com").downcase
-        "http://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
-    end
+    # def avatar_url(user)
+    #     gravatar_id = Digest::MD5::hexdigest("varavaravarvarm@gmail.com").downcase
+    #     "http://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
+    # end
 
     def toastr_flash
     flash.each_with_object([]) do |(type, message), flash_messages|
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,16 +10,14 @@ require("channels")
 require('jquery')
 require('popper.js')
 require('bootstrap')
+global.toastr = require("toastr")
 // require("jquery-ui")
 
 // import '@fortawesome/fontawesome-svg-core';
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-global.toastr = require("toastr")
+
 // FontAwesome.config.mutateApproach = 'sync'
 import "@fortawesome/fontawesome-free/js/all";
 
